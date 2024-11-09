9a3a213c55f0358e058ee1ffbf813243d1b7cb55
bsmtm0316 <taemin.chess@gmail.com>
2024-11-09 14:33:41 +0000

    Sat, Nov 9, 2024, 2:33 PM +00:00

--- calculator.c
+++ calculator.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 #include <cs50.h>
 
-int add(void);
+int add(int a, int b);
 
 int main(void)
 {
     int x = get_int("x: ");
     int y = get_int("y: ");
 
-    int z = add();
+    int z = add(x, y);
     printf("%i\n", z);
 }
 
-int add(int x, int y)
+int add(int a, int b)
 {
     return x + y;
 }
