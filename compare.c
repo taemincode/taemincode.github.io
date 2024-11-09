99873dc938dd4c601f013c1b8650fde4f49274bb
bsmtm0316 <taemin.chess@gmail.com>
2024-11-09 13:24:24 +0000

    Sat, Nov 9, 2024, 1:24 PM +00:00

--- compare.c
+++ compare.c
@@ -10,8 +10,12 @@ int main(void)
     {
         printf("x is less than y\n");
     }
-    else
+    else if (x > y)
     {
-        printf("x is not less than y\n");
+        printf("x is greater than than y\n");
+    }
+    else if (x == y)
+    {
+        printf("x is same as y\n")
     }
 }
