a868c8ea42ae5767073f6d888dd8999a480bf32b
bsmtm0316 <taemin.chess@gmail.com>
2024-11-09 13:40:07 +0000

    Sat, Nov 9, 2024, 1:40 PM +00:00

--- agree.c
+++ agree.c
@@ -5,19 +5,11 @@ int main(void)
 {
     char c = get_char("Do you agree? ");
 
-    if (c == 'y')
+    if (c == 'y' || c == 'Y')
     {
         printf("Agreed.\n");
     }
-    else if (c == 'Y')
-    {
-        printf("Agreed.\n");
-    }
-    else if (c == 'n')
-    {
-        printf("Not agreed.\n");
-    }
-    else if (c == 'N')
+    else if (c == 'n' || c == 'N')
     {
         printf("Not agreed.\n");
     }
