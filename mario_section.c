d81b23ebdc6c8ce0a15c07ab9c50739ed43eb90e
bsmtm0316 <taemin.chess@gmail.com>
2024-11-10 14:40:11 +0000

    Sun, Nov 10, 2024, 3:40 PM +01:00

--- mario_section.c
+++ mario_section.c
@@ -4,6 +4,14 @@
 int main(void)
 {
     int height = get_int("Height: ");
+    print_row(4);
+}
 
-    
+void print_row(int legnth)
+{
+    for (int i = 0; i < length; i++)
+    {
+        printf("#");
+    }
+    printf("\n")
 }
