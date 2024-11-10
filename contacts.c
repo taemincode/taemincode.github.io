75840840df9657e5883d10fd29b6859a2b12c658
bsmtm0316 <taemin.chess@gmail.com>
2024-11-10 14:18:58 +0000

    Sun, Nov 10, 2024, 3:18 PM +01:00

--- contacts.c
+++ contacts.c
@@ -5,6 +5,9 @@ int main(void)
 {
     string name = get_string("What is the person's name? ");
     int age = get_int("What is %s's age? ", name);
-    string number = get_int("What is %s's phone number? ", name);
-    printf("Name: %s\nAge: %i\nPhone number: %s\n", name, age, number);
+    string number = get_string("What is %s's phone number? ", name);
+
+    printf("Name: %s\n", name);
+    printf("Age: %i\n", age);
+    printf("Phone number: %s\n", number);
 }
