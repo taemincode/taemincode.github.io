58322932935ea8232ed305802cf8edd52ab655bc
bsmtm0316 <taemin.chess@gmail.com>
2024-11-10 14:43:51 +0000

    Sun, Nov 10, 2024, 3:43 PM +01:00

--- mario_section.c
+++ mario_section.c
@@ -1,17 +1,23 @@
 #include <cs50.h>
 #include <stdio.h>
 
+void print_row(int legnth);
 int main(void)
 {
     int height = get_int("Height: ");
-    print_row(4);
+    int i = 0;
+    while(i < height)
+    {
+        print_row(i + 1);
+        i++;
+    }
 }
 
-void print_row(int legnth)
+void print_row(int length)
 {
     for (int i = 0; i < length; i++)
     {
         printf("#");
     }
-    printf("\n")
+    printf("\n");
 }
