d09357990c2f2f7deca738dd57247a83dade7fed
bsmtm0316 <taemin.chess@gmail.com>
2024-11-14 16:09:05 +0000

    Thu, Nov 14, 2024, 5:09 PM +01:00

--- pp1/prime/prime.c
+++ pp1/prime/prime.c
@@ -33,7 +33,7 @@ bool prime(int number)
     // TODO
     for (int i = 2; i < number; i++)
     {
-        if (number % i == 0)
+        if (number == 1 || number % i == 0)
         {
             return false;
         }
