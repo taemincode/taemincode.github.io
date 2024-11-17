43f950b207897c591fdaa3b31c09e41599493d8c
taemincode <taemin.chess@gmail.com>
2024-11-17 17:10:15 +0000

    Sun, Nov 17, 2024, 6:10 PM +01:00

--- week2/pset/scrabble/scrabble.c
+++ week2/pset/scrabble/scrabble.c
@@ -1,2 +1,24 @@
+#include <ctype.h>
 #include <cs50.h>
-#include
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    string player1 = get_string("Player 1: ");
+    string player2 = get_string("Player 2: ");
+
+}
+
+int points(string player)
+{
+    int points = 0;
+    player = toupper(player);
+    for(int i = 0, l = strlen(player); i < l; i++)
+    {
+        if ('A' <= player[i] <= 'Z')
+        {
+            
+        }
+    }
+}
