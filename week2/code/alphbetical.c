d0856c1c0f20d3410883d8e466ec3526403e4a19
taemincode <taemin.chess@gmail.com>
2024-11-17 14:15:45 +0000

    Sun, Nov 17, 2024, 3:15 PM +01:00

--- week2/code/alphbetical.c
+++ week2/code/alphbetical.c
@@ -1,8 +1,12 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <strlen.h>
 
 int main(void)
 {
     string phrase = get_string("Enter a phrase: ");
-    
+    for (int i = 0, lenght = strlen(phrase); i < length; i++)
+    {
+        
+    }
 }
