b1c71c566a329c2dd4cae56051e4a169b0a442b1
taemincode <taemin.chess@gmail.com>
2024-11-17 17:20:26 +0000

    Sun, Nov 17, 2024, 6:20 PM +01:00

--- week2/pset/scrabble/scrabble.c
+++ week2/pset/scrabble/scrabble.c
@@ -12,13 +12,38 @@ int main(void)
 
 int points(string player)
 {
-    int points = 0;
     player = toupper(player);
+
+    int points = 0;
     for(int i = 0, l = strlen(player); i < l; i++)
     {
-        if (player[i] = 'A' || player[i] = 'E' ||)
+        if (player[i] = 'A' || player[i] = 'E' || player[i] = 'I' || player[i] = 'L' || player[i] = 'N' || player[i] = 'O' || player[i] = 'R' || player[i] = 'S' || player[i] = 'T' || player[i] = 'U')
         {
-
+            points++;
+        }
+        else if (player[i] = 'D' || player[i] = 'G')
+        {
+            points += 2;
+        }
+        else if (player[i] = 'B' || player[i] = 'C' || player[i] = 'M' || player[i] = 'P')
+        {
+            points += 3;
+        }
+        else if (player[i] = 'F' || player[i] = 'H' || player[i] = 'V' || player[i] = 'W' || player[i] = 'Y')
+        {
+            points += 4;
+        }
+        else if (player[i] = 'K')
+        {
+            points += 5;
+        }
+        else if (player[i] = 'J' || player[i] = 'X')
+        {
+            points += 8;
+        }
+        else if (player[i] = 'Q' || player[i] = 'Z')
+        {
+            points += 10;
         }
     }
 }
