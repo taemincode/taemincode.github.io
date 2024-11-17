f7e3b84ceb6ea24516b5533cea078f447f710713
taemincode <taemin.chess@gmail.com>
2024-11-17 17:26:26 +0000

    Sun, Nov 17, 2024, 6:26 PM +01:00

--- week2/pset/scrabble/scrabble.c
+++ week2/pset/scrabble/scrabble.c
@@ -3,11 +3,25 @@
 #include <stdio.h>
 #include <string.h>
 
+int points(string player);
+
 int main(void)
 {
     string player1 = get_string("Player 1: ");
     string player2 = get_string("Player 2: ");
 
+    if (points(player1) > points(player2))
+    {
+        printf("Player 1 wins!\n");
+    }
+    else if (points(player1) < points(player2))
+    {
+        printf("Player 2 wins!\n");
+    }
+    else
+    {
+        printf("Tie!\n");
+    }
 }
 
 int points(string player)
@@ -17,33 +31,35 @@ int points(string player)
     int points = 0;
     for(int i = 0, l = strlen(player); i < l; i++)
     {
-        if (player[i] = 'A' || player[i] = 'E' || player[i] = 'I' || player[i] = 'L' || player[i] = 'N' || player[i] = 'O' || player[i] = 'R' || player[i] = 'S' || player[i] = 'T' || player[i] = 'U')
+        if (player[i] == 'A' || player[i] == 'E' || player[i] == 'I' || player[i] == 'L' || player[i] == 'N' || player[i] == 'O' || player[i] == 'R' || player[i] == 'S' || player[i] == 'T' || player[i] == 'U')
         {
             points++;
         }
-        else if (player[i] = 'D' || player[i] = 'G')
+        else if (player[i] == 'D' || player[i] == 'G')
         {
             points += 2;
         }
-        else if (player[i] = 'B' || player[i] = 'C' || player[i] = 'M' || player[i] = 'P')
+        else if (player[i] == 'B' || player[i] == 'C' || player[i] == 'M' || player[i] == 'P')
         {
             points += 3;
         }
-        else if (player[i] = 'F' || player[i] = 'H' || player[i] = 'V' || player[i] = 'W' || player[i] = 'Y')
+        else if (player[i] == 'F' || player[i] == 'H' || player[i] == 'V' || player[i] == 'W' || player[i] == 'Y')
         {
             points += 4;
         }
-        else if (player[i] = 'K')
+        else if (player[i] == 'K')
         {
             points += 5;
         }
-        else if (player[i] = 'J' || player[i] = 'X')
+        else if (player[i] == 'J' || player[i] == 'X')
         {
             points += 8;
         }
-        else if (player[i] = 'Q' || player[i] = 'Z')
+        else if (player[i] == 'Q' || player[i] == 'Z')
         {
             points += 10;
         }
     }
+
+    return points;
 }
