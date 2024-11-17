c583a2755f040b704bb9a7ec9639b4a514537d72
taemincode <taemin.chess@gmail.com>
2024-11-17 14:43:41 +0000

    Sun, Nov 17, 2024, 3:43 PM +01:00

--- week2/code/mario.c
+++ week2/code/mario.c
@@ -1,7 +1,8 @@
 #include <cs50.h>
-#include  <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, string argv[]);
+int main(int argc, string argv[])
 {
-    
+    int height = atoi(argv[1]);
 }
