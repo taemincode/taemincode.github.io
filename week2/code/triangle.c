37eb749ef7914ca3770314060ff97a6ba63e9e3b
taemincode <taemin.chess@gmail.com>
2024-11-17 15:27:49 +0000

    Sun, Nov 17, 2024, 4:27 PM +01:00

--- week2/code/triangle.c
+++ week2/code/triangle.c
@@ -3,9 +3,10 @@
 
 int main(void)
 {
-    int first_side = ("What is the length of the first side? \n");
-    int second_side = ("What is the length of the second side? \n");
-    int third_side = ("What is the length of the third side? \n");
+    float sides[3];
+    sides[0] = ("What is the length of the first side? \n");
+    sides[1] = ("What is the length of the second side? \n");
+    sides[2] = ("What is the length of the third side? \n");
 
     if (valid_triangle(first_side, second_side, thrid_side) == true)
     {
@@ -17,4 +18,25 @@ int main(void)
     }
 }
 
-bool valid_triangle()
+bool valid_triangle(float a[])
+{
+    //Checks if all three sides are  positive
+    if (side1 < 0 || side2 < 0 || side3 < 0)
+    {
+        return false;
+    }
+
+    //Finds out the largest side
+    float largest_side = 0;
+    for (int i = 0; i < 3; i++)
+    {
+        if (a[i] > largest_side)
+        {
+            largest_side = a[i];
+        }
+    }
+
+    //Finds out if it's a valid triangle
+    if (largest_side < )
+
+}
