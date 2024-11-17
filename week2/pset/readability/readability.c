1418641a1465d981233f9206f978bbb17889b0eb
taemincode <taemin.chess@gmail.com>
2024-11-17 19:04:00 +0000

    Sun, Nov 17, 2024, 8:04 PM +01:00

--- week2/pset/readability/readability.c
+++ week2/pset/readability/readability.c
@@ -11,7 +11,23 @@ int main(void)
 
     // Count the number of letters, words, and sentences in the text
     int letters = 0;
-    for (int i = 0, n = strlen(text); i++)
+    int words = 1;
+    int sentences = 0;
+    for (int i = 0, n = strlen(text); i < n; i++)
+    {
+        if ('A' <= text[i] <= 'Z' || 'a' <= text[i] <= 'z')
+        {
+            letters++;
+        }
+        if (text[i] == ' ')
+        {
+            words++;
+        }
+        if (text[i] == '.' || text[i] == '!' || text[i] == '?')
+        {
+            sentences++;
+        }
+    }
 
     // Compute the Coleman-Liau index
 
