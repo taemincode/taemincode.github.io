63295b012ed60151057740427afec9702e79695f
taemincode <taemin.chess@gmail.com>
2024-11-20 16:06:02 +0000

    Wed, Nov 20, 2024, 5:06 PM +01:00

--- week2/pset/substitution/substitution.c
+++ week2/pset/substitution/substitution.c
@@ -4,9 +4,12 @@
 #include <stdilib.h>
 #include <string.h>
 
+
+void valid(int length, string key);
+
 int main(int argc, string argv[])
 {
-
+    valid(argc, argv[1]);
 }
 
 void valid(int length, string key)
@@ -14,16 +17,37 @@ void valid(int length, string key)
     // If the format is invalid
     if (length != 2)
     {
-        exit(1)
+        exit(1);
     }
 
     // If the lenghth of the key is invalid
     if (strlen(key) != 26)
     {
-        exit(1)
+        exit(1);
     }
 
     // If the key is not alphabetic
-    if ()
+    if (isalpha(key) == false)
+    {
+        exit(1);
+    }
+
+    // If the key has same alphabets
+    string upper_key = toupper(key);
+    for (int i = 0; i < 26; i++)
+    {
+        int counter = 0;
+        for (int j = 0; j < l; j++)
+        {
+            if (key[i] == key[j])
+            {
+                counter++;
+            }
+        }
+        if (counter != 1)
+        {
+            exit(1);
+        }
+    }
 
 }
