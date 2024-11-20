6846f9fed8fb2fd679971a0f3da51c425a294147
taemincode <taemin.chess@gmail.com>
2024-11-20 15:57:43 +0000

    Wed, Nov 20, 2024, 4:57 PM +01:00

--- week2/pset/substitution/substitution.c
+++ week2/pset/substitution/substitution.c
@@ -1,6 +1,7 @@
 #include <cs50.h>
 #include <ctype.h>
 #include <stdio.h>
+#include <stdilib.h>
 #include <string.h>
 
 int main(int argc, string argv[])
@@ -8,4 +9,21 @@ int main(int argc, string argv[])
 
 }
 
+void valid(int length, string key)
+{
+    // If the format is invalid
+    if (length != 2)
+    {
+        exit(1)
+    }
 
+    // If the lenghth of the key is invalid
+    if (strlen(key) != 26)
+    {
+        exit(1)
+    }
+
+    // If the key is not alphabetic
+    if ()
+
+}
