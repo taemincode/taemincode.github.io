b04a3f9b81b66ce8da24e4f98b6cf45df5c5c935
taemincode <taemin.chess@gmail.com>
2024-11-21 15:13:59 +0000

    Thu, Nov 21, 2024, 4:13 PM +01:00

---
+++ week2/pp/no-vowels/no-vowels.c
@@ -0,0 +1,35 @@
+// Write a function to replace vowels with numbers
+// Get practice with strings
+// Get practice with command line
+// Get practice with switch
+
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, string argv[])
+{
+    '''
+    If the program is executed without any command-line
+    arguments or with more than one command-line argument
+    '''
+    if (argc != 2)
+    {
+        printf("Usage: ./no-vowels word\n");
+        return 1;
+    }
+
+    // Replaces the word
+
+}
+
+string replace(string string)
+{
+    for (i = 0, l = strlen(string); i < l; i++)
+    {
+        swich (string[i])
+        {
+            case
+        }
+    }
+}
