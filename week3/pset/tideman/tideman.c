25adb9fcfe03d432ed4a8537ce9a2734184de717
taemincode <taemin.chess@gmail.com>
2024-11-24 12:43:57 +0000

    Sun, Nov 24, 2024, 1:43 PM +01:00

--- week3/pset/tideman/tideman.c
+++ week3/pset/tideman/tideman.c
@@ -116,7 +116,7 @@ void record_preferences(int ranks[])
     // TODO
     for (int i = 0; i < candidate_count; i++)
     {
-        for (int j = 0; j < candidate_count - i; j++)
+        for (int j = i + 1; j < candidate_count; j++)
         {
 
                 preferences[rank[i]][j]++;
