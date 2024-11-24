c0a0aa150c67e8a3f5451715c0fe020c7ab0bc93
taemincode <taemin.chess@gmail.com>
2024-11-24 09:56:07 +0000

    Sun, Nov 24, 2024, 10:56 AM +01:00

---
+++ week3/code/collatz.c
@@ -0,0 +1,33 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int collatz(int n);
+
+int main(void)
+{
+    int n;
+    do
+    {
+        n = get_int("Number: ");
+    }
+    while(n < 1);
+
+    printf("%i steps\n", collatz(n));
+}
+
+int collatz(int n)
+{
+    if (n == 1)
+        return 0;
+    else
+    {
+        if (n % 2  == 0)
+        {
+            return 1 + collatz(n / 2);
+        }
+        else
+        {
+            return 1 + collatz(3 * n + 1);
+        }
+    }
+}
