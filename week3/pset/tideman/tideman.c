62c5837853f953e80a1790fc1a728d9af0e4aa56
taemincode <taemin.chess@gmail.com>
2024-11-24 13:06:54 +0000

    Sun, Nov 24, 2024, 2:06 PM +01:00

--- week3/pset/tideman/tideman.c
+++ week3/pset/tideman/tideman.c
@@ -148,7 +148,19 @@ void add_pairs(void)
 void sort_pairs(void)
 {
     // TODO
-    
+    for (int i = 0; i < candidates; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (array[j] < array[j + 1])
+            {
+                // Swap array[j] and array[j + 1]
+                int temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
     return;
 }
 
