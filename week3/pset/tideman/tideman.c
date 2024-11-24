ce880853f2223bb3e69af1c0e6c5fc5ed3213e0b
taemincode <taemin.chess@gmail.com>
2024-11-24 12:50:31 +0000

    Sun, Nov 24, 2024, 1:50 PM +01:00

--- week3/pset/tideman/tideman.c
+++ week3/pset/tideman/tideman.c
@@ -129,7 +129,16 @@ void record_preferences(int ranks[])
 void add_pairs(void)
 {
     // TODO
-    
+    for (int i = 0; i < candidate_count; i++)
+    {
+        for (int j = i + 1; j < candidate_count; j++)
+        {
+            if (preferences[i][j] > preferences[j][i])
+            {
+                pair.winner
+            }
+        }
+    }
     return;
 }
 
