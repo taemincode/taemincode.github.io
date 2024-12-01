2687432ea102f803d87f3530aa456a6ae228fd05
taemincode <taemin.chess@gmail.com>
2024-12-01 09:18:28 +0000

    Sun, Dec 1, 2024, 10:18 AM +01:00

--- week4/code/pdf.c
+++ week4/code/pdf.c
@@ -1,7 +1,19 @@
 #include <cs50.h>
 #include <stdio.h>
 
+typedef unit_8 
+
 int main(int argc, char *argv[])
 {
-    
+    if (argc != 2)
+    {
+        printf("Usage: ./pdf file_name")
+        return 1;
+    }
+
+    FILE *f = fopen(argv[1], "r")
+
+    char *buffer = malloc(4 * sizeof(char));
+
+    fread(buffer, 1BYTE)
 }
