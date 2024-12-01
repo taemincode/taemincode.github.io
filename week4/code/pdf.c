e76d7ad966a5b088ea2e4ffbd7e225811f0b70a5
taemincode <taemin.chess@gmail.com>
2024-12-01 09:23:26 +0000

    Sun, Dec 1, 2024, 10:23 AM +01:00

--- week4/code/pdf.c
+++ week4/code/pdf.c
@@ -1,19 +1,29 @@
 #include <cs50.h>
 #include <stdio.h>
 
-typedef unit_8 
-
 int main(int argc, char *argv[])
 {
     if (argc != 2)
     {
-        printf("Usage: ./pdf file_name")
+        printf("Usage: ./pdf file_name\n");
         return 1;
     }
 
-    FILE *f = fopen(argv[1], "r")
+    FILE *f = fopen(argv[1], "r");
 
-    char *buffer = malloc(4 * sizeof(char));
+    int *buffer = malloc(4 * sizeof(int));
 
-    fread(buffer, 1BYTE)
+    fread(buffer, 1, 4, argv[1]);
+    if (buffer[0] == 37, buffer[1] == 80, buffer[2] == 68, buffer[3] == 70)
+    {
+        printf("Is a PDF file\n");
+        return 0;
+    }
+    else
+    {
+        printf("Is not a PDF file\n");
+        return 0;
+    }
+
+    free(buffer);
 }
