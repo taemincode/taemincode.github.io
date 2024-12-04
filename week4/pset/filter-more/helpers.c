d6bbe40f2c7ed7ba1cc30f17e528684fe7c8f237
taemincode <taemin.chess@gmail.com>
2024-12-04 17:24:42 +0000

    Wed, Dec 4, 2024, 6:24 PM +01:00

--- week4/pset/filter-more/helpers.c
+++ week4/pset/filter-more/helpers.c
@@ -54,7 +54,18 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
     for (int = i; i < height; i++)
     {
         for (int = j; j < width; j++)
-        
+        {
+            RGBTRIPLE avg_value;
+            bool h_edge_u = false;
+            bool h_edge_d = false;
+
+            avg_value.rgbtRed += image[i][j].rgbtRed;
+            if (i != 1)
+            {
+                if (j != 1)
+                    avg_value.rgbtRed += image[i - 1][].rgbtRed + imgae[i]
+            }
+        }
     }
 
     return;
