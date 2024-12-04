e57778d58f7659faa666371e04ecfa2d3b105001
taemincode <taemin.chess@gmail.com>
2024-12-04 17:00:08 +0000

    Wed, Dec 4, 2024, 6:00 PM +01:00

--- week4/pset/filter-more/helpers.c
+++ week4/pset/filter-more/helpers.c
@@ -21,8 +21,6 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
-    int half;
-
     if (width % 2 == 0)
         int half = width / 2;
     else
