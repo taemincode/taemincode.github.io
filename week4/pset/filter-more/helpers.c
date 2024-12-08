84cac258747425acf1c6f543d93e91817b40a34f
taemincode <taemin.chess@gmail.com>
2024-12-08 07:56:19 +0000

    Sun, Dec 8, 2024, 8:56 AM +01:00

--- week4/pset/filter-more/helpers.c
+++ week4/pset/filter-more/helpers.c
@@ -196,5 +196,152 @@ int get_color_value(RGBTRIPLE pixel, char color)
 // Detect edges
 void edges(int height, int width, RGBTRIPLE image[height][width])
 {
+    RGBTRIPLE tmp[height][width];
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            tmp[i][j].rgbtRed = edge(height, width, image, i, j, 'R');
+            tmp[i][j].rgbtGreen = edge(height, width, image, i, j, 'G');
+            tmp[i][j].rgbtBlue = edge(height, width, image, i, j, 'B');
+        }
+    }
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            image[i][j].rgbtRed = tmp[i][j].rgbtRed;
+            image[i][j].rgbtGreen = tmp[i][j].rgbtGreen;
+            image[i][j].rgbtBlue = tmp[i][j].rgbtBlue;
+        }
+    }
+
     return;
+}
+
+int sum_color(int height, int width, RGBTRIPLE image[height][width], int i, int j, char color)
+{
+    int gx = 0;
+    int gy = 0;
+
+    if (i == 0)
+    {
+        if (j == 0)
+        {
+            gx = get_color_value(image[i][j], color) * 0 +
+                   get_color_value(image[i][j + 1], color) * 2 +
+                   get_color_value(image[i + 1][j], color) * 0+
+                   get_color_value(image[i + 1][j + 1], color) * ;
+            num = 4;
+        }
+        else if (j == width - 1)
+        {
+            sum += get_color_value(image[i][j], color) +
+                   get_color_value(image[i][j - 1], color) +
+                   get_color_value(image[i + 1][j - 1], color) +
+                   get_color_value(image[i + 1][j], color);
+            num = 4;
+        }
+        else
+        {
+            sum += get_color_value(image[i][j], color) +
+                   get_color_value(image[i][j - 1], color) +
+                   get_color_value(image[i][j + 1], color) +
+                   get_color_value(image[i + 1][j - 1], color) +
+                   get_color_value(image[i + 1][j], color) +
+                   get_color_value(image[i + 1][j + 1], color);
+            num = 6;
+        }
+    }
+    else if (i == height - 1)
+    {
+        if (j == 0)
+        {
+            sum += get_color_value(image[i][j], color) +
+                   get_color_value(image[i][j + 1], color) +
+                   get_color_value(image[i - 1][j], color) +
+                   get_color_value(image[i - 1][j + 1], color);
+            num = 4;
+        }
+        else if (j == width - 1)
+        {
+            sum += get_color_value(image[i][j], color) +
+                   get_color_value(image[i][j - 1], color) +
+                   get_color_value(image[i - 1][j - 1], color) +
+                   get_color_value(image[i - 1][j], color);
+            num = 4;
+        }
+        else
+        {
+            sum += get_color_value(image[i][j], color) +
+                   get_color_value(image[i][j - 1], color) +
+                   get_color_value(image[i][j + 1], color) +
+                   get_color_value(image[i - 1][j - 1], color) +
+                   get_color_value(image[i - 1][j], color) +
+                   get_color_value(image[i - 1][j + 1], color);
+            num = 6;
+        }
+    }
+    else
+    {
+        if (j == 0)
+        {
+            sum += get_color_value(image[i + 1][j], color) +
+                   get_color_value(image[i + 1][j + 1], color) +
+                   get_color_value(image[i][j], color) +
+                   get_color_value(image[i][j + 1], color) +
+                   get_color_value(image[i - 1][j], color) +
+                   get_color_value(image[i - 1][j + 1], color);
+            num = 6;
+        }
+        else if (j == width - 1)
+        {
+            sum += get_color_value(image[i + 1][j - 1], color) +
+                   get_color_value(image[i + 1][j], color) +
+                   get_color_value(image[i][j - 1], color) +
+                   get_color_value(image[i][j], color) +
+                   get_color_value(image[i - 1][j - 1], color) +
+                   get_color_value(image[i - 1][j], color);
+            num = 6;
+        }
+        else
+        {
+            sum += get_color_value(image[i + 1][j - 1], color) +
+                   get_color_value(image[i + 1][j], color) +
+                   get_color_value(image[i + 1][j + 1], color) +
+                   get_color_value(image[i][j - 1], color) +
+                   get_color_value(image[i][j], color) +
+                   get_color_value(image[i][j + 1], color) +
+                   get_color_value(image[i - 1][j - 1], color) +
+                   get_color_value(image[i - 1][j], color) +
+                   get_color_value(image[i - 1][j + 1], color);
+            num = 9;
+        }
+    }
+
+    return round((double)sum / (double)num);
+}
+
+int get_color_value(RGBTRIPLE pixel, char color)
+{
+    if (color == 'R')
+        return pixel.rgbtRed;
+    else if (color == 'G')
+        return pixel.rgbtGreen;
+    else
+        return pixel.rgbtBlue;
+}
+
+int gx(int top_left, int top, int top_right, int left, int middle, int right, int bottom_right, int bottom, int bottom_left)
+{
+    int gx = top_left * (-1) + top * 0 + top_right * (1) left * (-2) + middle * 0 + right * 2 + bottom_right * (-1) + bottom * 0 + bottom_left * 1;
+
+    if (gx > 255)
+    {
+        gx = 255;
+    }
+
+    return 255;
 }
