4ab9db671219f542b72871d594d5153e2a8970c9
taemincode <taemin.chess@gmail.com>
2024-12-09 09:52:04 +0000

    Mon, Dec 9, 2024, 10:52 AM +01:00

--- week4/recover/recover.c
+++ week4/recover/recover.c
@@ -1,12 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef uin BYTE
+typedef uint8_t BYTE
 
 int main(int argc, char *argv[])
 {
     FILE *f = fopen(card, "r");
-    int buff
+    BYTE buff[512];
 
-    while (fread())
+    while (fread(buff, 512, 1, f) != 512)
+    {
+        fread(buff, 512, 1, f)
+        int file_count == 0;
+
+        if (buff[0] == 0xff && buff[1] == 0xd8 && buff[2] == 0xff && (buff[3] & 0xf0) == 0xe0)
+        {
+            if (file_count == 0)
+            {
+                sprintf()
+            }
+            else
+            {
+
+            }
+        }
+    }
 }
