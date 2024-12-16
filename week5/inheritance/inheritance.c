a948a29646c1b57e3ac2ed2a5ac25228413ff12f
taemincode <taemin.chess@gmail.com>
2024-12-16 16:08:58 +0000

    Mon, Dec 16, 2024, 5:08 PM +01:00

--- week5/inheritance/inheritance.c
+++ week5/inheritance/inheritance.c
@@ -39,7 +39,11 @@ int main(void)
 person *create_family(int generations)
 {
     // TODO: Allocate memory for new person
-    
+    person *new_person = malloc(sizeof(person));
+    if (new_person == NULL)
+    {
+        return
+    }
 
     // If there are still generations left to create
     if (generations > 1)
