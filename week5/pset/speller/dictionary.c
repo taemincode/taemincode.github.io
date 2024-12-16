2fca30c45ff3d92f6be5a1d1322ddeb4d0351bba
taemincode <taemin.chess@gmail.com>
2024-12-16 18:58:01 +0000

    Mon, Dec 16, 2024, 7:58 PM +01:00

--- week5/pset/speller/dictionary.c
+++ week5/pset/speller/dictionary.c
@@ -17,7 +17,7 @@ typedef struct node
 const unsigned int N = 26;
 
 // Hash table
-node *table[N];
+node *table[N][N][N];
 
 // Returns true if word is in dictionary, else false
 bool check(const char *word)
