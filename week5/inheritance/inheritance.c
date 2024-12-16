9f9cc385f28e0c1245783ccf094a53a72b4b0c23
taemincode <taemin.chess@gmail.com>
2024-12-16 16:18:27 +0000

    Mon, Dec 16, 2024, 5:18 PM +01:00

--- week5/inheritance/inheritance.c
+++ week5/inheritance/inheritance.c
@@ -58,7 +58,7 @@ person *create_family(int generations)
         new_person->parents[1] = parent1;
 
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
-        new_person->alleles[0] = parent0->alleles[]
+        new_person->alleles[0] = parent0->alleles[rand() % 2];
 
     }
 
