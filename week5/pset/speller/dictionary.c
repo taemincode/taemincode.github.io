da5ab4480005f7df346c03e40a2646f2310e1630
taemincode <taemin.chess@gmail.com>
2024-12-16 16:58:22 +0000

    Mon, Dec 16, 2024, 5:58 PM +01:00

--- week5/pset/speller/dictionary.c
+++ week5/pset/speller/dictionary.c
@@ -2,6 +2,7 @@
 
 #include <ctype.h>
 #include <stdbool.h>
+#include <stdio.h>
 
 #include "dictionary.h"
 
@@ -36,6 +37,12 @@ unsigned int hash(const char *word)
 bool load(const char *dictionary)
 {
     // TODO
+    FILE *dictionary = fopen(dictionary, "r");
+    if (dictionary == NULL)
+    {
+        return false;
+    }
+
     
 }
 
