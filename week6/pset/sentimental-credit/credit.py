bc71e5fee90994bd349e4df60539bd893b3073ef
taemincode <taemin.chess@gmail.com>
2024-12-20 20:17:28 +0000

    Fri, Dec 20, 2024, 9:17 PM +01:00

--- week6/pset/sentimental-credit/credit.py
+++ week6/pset/sentimental-credit/credit.py
@@ -12,7 +12,20 @@ def main():
 
 def luhn(number):
     # If Amex
-    if
+    if digit == 15:
+        if
 
 
-def 
+def digit(number)
+    return len(str(number))
+
+
+def square(base, exponent):
+    if exponent == 1:
+        return base
+    else:
+        return square(base, exponent - 1) * base
+
+
+def starting(number, digit):
+    return number - number % square(10, digit)
