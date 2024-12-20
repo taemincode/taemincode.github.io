f60908b855248d25b4f453339dde9ebf7fee72fb
taemincode <taemin.chess@gmail.com>
2024-12-20 16:59:11 +0000

    Fri, Dec 20, 2024, 5:59 PM +01:00

--- week6/code/phonebook.py
+++ week6/code/phonebook.py
@@ -2,9 +2,7 @@ names = ["Carter", "David", "John"]
 
 name = input("Name: ")
 
-for n in names:
-    if name == n:
-        print("Found")
-        break
+if name in names:
+    print("Found")
 else:
     print("Not found")
