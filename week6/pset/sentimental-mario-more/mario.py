195a1f487ae44a1274bf7afb813051752610c3b5
taemincode <taemin.chess@gmail.com>
2024-12-20 19:17:49 +0000

    Fri, Dec 20, 2024, 8:17 PM +01:00

--- week6/pset/sentimental-mario-more/mario.py
+++ week6/pset/sentimental-mario-more/mario.py
@@ -2,4 +2,26 @@ from cs50 import get_int
 
 
 def main():
-    height = get_int("Height: ")
+    while True:
+        height = get_int("Height: ")
+        if height > 1:
+            break
+    for i in range(height):
+        row(height - (i + 1), i + 1)
+        print(" ")
+
+
+def row(space, brick):
+    for i in range(space):
+        print(" ", end="")
+    brick_f(brick)
+    print(" ", end="")
+    brick_f(brick)
+
+
+def brick_f(number_of_bricks):
+    for i in range(number_of_bricks):
+        print("#", end="")
+
+
+main()
