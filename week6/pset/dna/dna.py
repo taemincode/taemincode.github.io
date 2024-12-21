8b704e412ee2e7384c6246975c4e6fb74aafb8eb
taemincode <taemin.chess@gmail.com>
2024-12-21 14:30:39 +0000

    Sat, Dec 21, 2024, 3:30 PM +01:00

--- week6/pset/dna/dna.py
+++ week6/pset/dna/dna.py
@@ -18,9 +18,10 @@ def main():
 
     # TODO: Read DNA sequence file into a variable
     f = open(argv[3], 'r')
+    sequence = f.readline()
 
     # TODO: Find longest match of each STR in DNA sequence
-
+    
     # TODO: Check database for matching profiles
 
     return
