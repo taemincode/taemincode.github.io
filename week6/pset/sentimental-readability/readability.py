b865de7bb5d416674760b1559c7bd1767511180f
taemincode <taemin.chess@gmail.com>
2024-12-21 07:38:52 +0000

    Sat, Dec 21, 2024, 8:38 AM +01:00

---
+++ week6/pset/sentimental-readability/readability.py
@@ -0,0 +1,15 @@
+from cs50 import get_string
+
+
+def main():
+    # Prompt the user for some text
+    text = get_string("Text: ")
+
+    # Count the number of letters, words, and sentences in the text
+    letters = 0
+    words = 1
+    sentences = 0
+    for i in range(text):
+        if 'A' <= text[i] && text[i] <= 'Z') || ('a' <= text[i] && text[i] <= 'z'
+            letters += 1
+        elif
