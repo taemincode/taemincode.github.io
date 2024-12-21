8a494cd2db6504936a98dd018bf3c3b5625ff47e
taemincode <taemin.chess@gmail.com>
2024-12-21 14:18:25 +0000

    Sat, Dec 21, 2024, 3:18 PM +01:00

--- week6/pset/dna/dna.py
+++ week6/pset/dna/dna.py
@@ -10,7 +10,12 @@ def main():
         return
 
     # TODO: Read database file into a variable
-    
+    rows = []
+    with open("foo.csv") as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            rows.append(row)
+            
     # TODO: Read DNA sequence file into a variable
 
     # TODO: Find longest match of each STR in DNA sequence
