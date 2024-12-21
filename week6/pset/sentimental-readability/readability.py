c87d70126490be5748f96af73369fe67ef1ce00d
taemincode <taemin.chess@gmail.com>
2024-12-21 07:47:01 +0000

    Sat, Dec 21, 2024, 8:47 AM +01:00

--- week6/pset/sentimental-readability/readability.py
+++ week6/pset/sentimental-readability/readability.py
@@ -9,7 +9,25 @@ def main():
     letters = 0
     words = 1
     sentences = 0
-    for i in range(text):
-        if 'A' <= text[i] && text[i] <= 'Z') || ('a' <= text[i] && text[i] <= 'z'
+    for i in range(len(text)):
+        if 'A' <= text[i] and text[i] <= 'Z' or 'a' <= text[i] and text[i] <= 'z':
             letters += 1
-        elif
+        elif text[i] == ' ':
+            words += 1
+        elif text[i] == '.' or text[i] == '!' or text[i] == '?':
+            sentences += 1
+
+    l = letters / words * 100
+    s = sentences / words * 100;
+
+    index = 0.0588 * l - 0.296 * s - 15.8
+
+    if index >= 1 and index < 16:
+        print(f"Grade {round(index)}")
+    elif index >= 16:
+        print("Grade 16+")
+    else:
+        print("Before Grade 1")
+
+
+main()
