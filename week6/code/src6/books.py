cb1f6c26d855f324f484c031202a1013994c4b17
taemincode <taemin.chess@gmail.com>
2024-12-22 10:35:35 +0000

    Sun, Dec 22, 2024, 11:35 AM +01:00

--- week6/code/src6/books.py
+++ week6/code/src6/books.py
@@ -2,8 +2,9 @@ books = []
 
 for i in range(3):
     book = {
-    "author" : input("Enter an author: ")
-    "title" : input("Enter a title: ")}
+        "author": input("Enter an author: "),
+        "title": input("Enter a title: ")
+    }
     books.append(book)
 
 for book in books:
