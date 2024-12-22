6188e8bb97c27bc7eb07d7ffeadbec5b29926b5f
taemincode <taemin.chess@gmail.com>
2024-12-22 11:16:51 +0000

    Sun, Dec 22, 2024, 12:16 PM +01:00

---
+++ week6/pp/bank.py
@@ -0,0 +1,17 @@
+from cs50 import get_string
+
+
+def main():
+    greeting = get_string("Greeting: ").lower
+
+    if "hello" in greeting:
+        print("$0")
+    elif "h" in greeting:
+        print("$20")
+    else:
+        print("$100")
+
+    return
+
+
+main()
