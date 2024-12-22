556be7c1a68b5cf0f8fa2fab80ed2c820ee442c1
taemincode <taemin.chess@gmail.com>
2024-12-22 11:34:14 +0000

    Sun, Dec 22, 2024, 12:34 PM +01:00

--- week6/pp/figlet/figlet.py
+++ week6/pp/figlet/figlet.py
@@ -1,6 +1,24 @@
 from pyfiglet import Figlet
+import sys
 
-figlet = Figlet()
-figlet.getFonts()
-figlet.setFont(font=f)
-print(figlet.renderText(s))
+
+def main()
+    figlet = Figlet()
+    figlet.getFonts()
+
+    if len(sys.argv) == 2
+    {
+        if argv[1] not in ['-f', '--font']:
+            print("Invailid usage")
+            sys.exit
+
+        figlet.setFont(font=argv[3])
+    }
+    elif len(sys.argv) == 0
+    {
+        print("Invalid)
+    }
+
+    figlet.setFont(font=f)
+
+    print(figlet.renderText(s))
