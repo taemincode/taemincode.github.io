a3cd98ce42f8be536b40b86aa1371bfdca126579
taemincode <taemin.chess@gmail.com>
2024-12-22 11:20:28 +0000

    Sun, Dec 22, 2024, 12:20 PM +01:00

--- week6/pp/bank.py
+++ week6/pp/bank.py
@@ -4,9 +4,9 @@ from cs50 import get_string
 def main():
     greeting = get_string("Greeting: ").lower()
 
-    if 'hello' in greeting:
+    if greeting[0:5] == 'hello':
         print("$0")
-    elif 'h' in greeting:
+    elif greeting[0] == 'h':
         print("$20")
     else:
         print("$100")
